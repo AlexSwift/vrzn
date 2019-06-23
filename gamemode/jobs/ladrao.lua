6f7435484a7a6226e900b1aef1c086c973e4a564
henriquecgarcia <henriquecampanhagarcia@gmail.com>
2019-06-23 01:07:18 -0300

    Job de Ladrão criada

---
+++ gamemode/jobs/ladrao.lua
@@ -0,0 +1,52 @@
+--[[
+	Name: ladrao.lua
+	For: TalosLife
+	By: TalosLife
+]]--
+
+local Job = {}
+Job.ID = 1
+Job.Enum = "JOB_ASSALTANTE"
+Job.TeamColor = Color( 255, 255, 255, 255 )
+Job.Name = "Ladrão"
+Job.Pay = {
+	{ PlayTime = 0, Pay = 0 },
+}
+
+function Job:OnPlayerJoinJob( pPlayer )
+end
+
+function Job:OnPlayerQuitJob( pPlayer )
+end
+
+function Job:GetPlayerModel( pPlayer )
+	local base, overload
+
+	if SERVER then
+		base = GAMEMODE.Player:GetGameVar( pPlayer, "char_model_base", nil )
+		overload = GAMEMODE.Player:GetGameVar( pPlayer, "char_model_overload", nil )
+	else
+		base = GAMEMODE.Player:GetGameVar( "char_model_base", nil )
+		overload = GAMEMODE.Player:GetGameVar( "char_model_overload", nil )
+	end
+	
+	if util.IsValidModel( overload or "" ) then
+		return overload
+	else
+		return base
+	end
+end
+
+function Job:PlayerSetModel( pPlayer )
+	local mdl = self:GetPlayerModel(pPlayer)
+
+	if mdl then
+		pPlayer:SetModel( mdl )
+		pPlayer:SetSkin( GAMEMODE.Player:GetGameVar(pPlayer, "char_skin", 0) )
+	end
+end
+
+function Job:PlayerLoadout( pPlayer )
+end
+
+GM.Jobs:Register( Job )
