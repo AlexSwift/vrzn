38a1af4b67cc1afa69c3e2dff03e043620a74ceb
Matheus alves <matheus.silva3@ftc.edu.br>
2019-06-27 02:32:59 -0300

    Testando uma função no rolê

--- gamemode/cl_needs.lua
+++ gamemode/cl_needs.lua
@@ -31,3 +31,11 @@ hook.Add( "GamemodeDefineGameVars", "DefineNeedVars", function( pPlayer )
 		GAMEMODE.Player:DefineGameVar( "need_".. k, v.Max, "UInt16" )
 	end
 end )
+
+
+
+
+
+
+
+-- print(  LocalPlayer():GetGamemodeData().SharedGameVars["eq_slot_Head"].Value )
