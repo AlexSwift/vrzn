4ab117193eeeb008dfb91a016fb8bae879e0019c
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-16 15:43:31 -0300

    Preparação pro sistema de amigos.

--- gamemode/modules/friends/cl_buddies.lua
+++ gamemode/modules/friends/cl_buddies.lua
@@ -1,40 +1,43 @@
 --[[
 	Name: cl_buddies.lua
-	For: TalosLife
-	By: Rustic7
-]]--
+		by: Asriel + CodeRed
 
-surface.CreateFont( "BuddyFont", {size = 128, weight = 500, font = "DermaLarge"} )
+]]
+--
+surface.CreateFont("BuddyFont", {
+	size = 128,
+	weight = 500,
+	font = "DermaLarge"
+})
 
 GM.Buddy = (GAMEMODE or GM).Buddy or {}
 GM.Buddy.m_tblCurBuddies = (GAMEMODE or GM).Buddy.m_tblCurBuddies or {}
-GM.Buddy.m_matBuddyIcon = Material( "icon16/user.png", "smooth" )
+--GM.Buddy.m_matBuddyIcon = Material("icon16/user.png", "smooth")
 
-function GM.Buddy:GetPlayerBuddyID( pPlayer )
+function GM.Buddy:GetPlayerBuddyID(pPlayer)
 	if not pPlayer:GetCharacterID() then return end
-	
-	local buddyID = pPlayer:GetCharacterID()
+	local buddyID = pPlayer:SteamID()
+
 	return self.m_tblCurBuddies[buddyID] and buddyID or nil
 end
 
-function GM.Buddy:GetPlayerByBuddyID( intBuddyID )
-	for k, v in pairs( player.GetAll() ) do
-		if v:GetCharacterID() == intBuddyID then
-			return v
-		end
+function GM.Buddy:GetPlayerByBuddyID(intBuddyID)
+	for k, v in pairs(player.GetAll()) do
+		if v:SteamID() == intBuddyID then return v end
 	end
 end
 
-function GM.Buddy:GetBuddyData( intBuddyID )
+function GM.Buddy:GetBuddyData(intBuddyID)
 	return self.m_tblCurBuddies[intBuddyID]
 end
 
-function GM.Buddy:IsBuddyWith( pPlayer )
-	if not pPlayer:GetCharacterID() then return false end
-	return self.m_tblCurBuddies[pPlayer:GetCharacterID()] or false
+function GM.Buddy:IsBuddyWith(pPlayer)
+	if not IsValid(pPlayer) then return false end
+
+	return self.m_tblCurBuddies[pPlayer:SteamID()] or false
 end
 
-function GM.Buddy:SetBuddyTable( tblBuddies )
+function GM.Buddy:SetBuddyTable(tblBuddies)
 	self.m_tblCurBuddies = tblBuddies
 end
 
@@ -42,26 +45,35 @@ function GM.Buddy:GetBuddyTable()
 	return self.m_tblCurBuddies
 end
 
-function GM.Buddy:PaintBuddyCard( pPlayer )
-	surface.SetFont( "BuddyFont" )
-	local tW, tH = surface.GetTextSize( pPlayer:Nick() )
-	surface.SetTextColor( 255, 255, 255, 255 )
-	surface.SetTextPos( -(tW /2), tH /2 )
-	surface.DrawText( pPlayer:Nick() )
+function GM.Buddy:PaintBuddyCard(pPlayer)
+	if (pPlayer:GetMoveType() == MOVETYPE_NOCLIP) then return end
+	surface.SetFont("BuddyFont")
 
-	local iconSize = 128
-	surface.SetMaterial( self.m_matBuddyIcon )
-	surface.SetDrawColor( 255, 255, 255, 255 )
-	surface.DrawTexturedRect( -(tW /2) -iconSize -24, (tH /2), iconSize, iconSize )
+	if pPlayer:IsPolice() and LocalPlayer():IsPolice() then
+		local tW, tH = surface.GetTextSize(pPlayer:Nick() .. " - " .. GAMEMODE.PoliceRanks:GetPrettyRank(pPlayer))
+		surface.SetTextColor(255, 255, 255, 255)
+		surface.SetTextPos(-(tW / 2), tH / 2)
+		surface.DrawText(pPlayer:Nick() .. " - " .. GAMEMODE.PoliceRanks:GetPrettyRank(pPlayer))
+	else
+--		local tW, tH = surface.GetTextSize(pPlayer:Nick())
+--		surface.SetTextColor(255, 255, 255, 255)
+--		surface.SetTextPos(-(tW / 2), tH / 2)
+--		surface.DrawText(pPlayer:Nick())
+--		local iconSize = 128
+--		surface.SetMaterial(self.m_matBuddyIcon)
+--		surface.SetDrawColor(255, 255, 255, 255)
+--		surface.DrawTexturedRect(-(tW / 2)+340 - iconSize - 24, (tH / 2), iconSize, iconSize)
+	end
 end
 
-function GM.Buddy:PaintNameTag( pPlayer )
-	surface.SetFont( "BuddyFont" )
-	local tW, tH = surface.GetTextSize( pPlayer:Nick() )
-	surface.SetTextColor( 255, 255, 255, 255 )
-	surface.SetTextPos( -(tW /2), tH /2 )
-	surface.DrawText( pPlayer:Nick() )
-end
+--function GM.Buddy:PaintNameTag(pPlayer)
+--	surface.SetFont("BuddyFont")
+--	local tW, tH = surface.GetTextSize(pPlayer:Nick())
+--	surface.SetTextColor(255, 255, 255, 255)
+--	surface.SetTextPos(-(tW / 2), tH / 2)
+	-- surface.DrawText(pPlayer:Nick())
+--end
+ 
 
 function GM.Buddy:PostDrawTranslucentRenderables()
 	local pos, offset = nil, Vector( 0, 0, 50 )
@@ -72,18 +84,18 @@ function GM.Buddy:PostDrawTranslucentRenderables()
 		if v == LocalPlayer() then continue end
 		
 		if self:IsBuddyWith( v ) then
-			ent =  v
+			parceiro = true
+			ent = IsValid( v:GetRagdoll() ) and v:GetRagdoll() or v
 			if ent:GetPos():DistToSqr( myPos ) > GAMEMODE.Config.RenderDist_Level2 ^2 then continue end
 			pos = ent:LocalToWorld( ent:OBBCenter() ) +offset
 			cam.Start3D2D( pos, ang, 0.035 )
 				self:PaintBuddyCard( v )
 			cam.End3D2D()
 		else
-			ent =  v
+			ent = IsValid( v:GetRagdoll() ) and v:GetRagdoll() or v
 			if ent:GetPos():DistToSqr( myPos ) > 350 ^2 then continue end
 			pos = ent:LocalToWorld( ent:OBBCenter() ) +offset
 			cam.Start3D2D( pos, ang, 0.035 )
-				self:PaintNameTag( v )
 			cam.End3D2D()
 		end
 	end
--- gamemode/modules/friends/sv_buddies.lua
+++ gamemode/modules/friends/sv_buddies.lua
@@ -1,20 +1,17 @@
 --[[
 	Name: sv_buddies.lua
------------------------------------------------------------------
--- @package     VrZn - Custom Gamemode (SRP BASE)
--- @author     Nodge
--- @build       Beta 1
------------------------------------------------------------------
+	For: TalosLife
+	By: TalosLife
 ]]--
 
-
+util.AddNetworkString("santos_askbuddy")-- Send Friend Request
 GM.Buddy = {}
 
 function GM.Buddy:GetPlayerBuddyID( pPlayer, pOther )
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not saveTable.Buddies then return end
 
-	local buddyID = pOther:GetCharacterID()
+	local buddyID = pOther:SteamID()
 	if saveTable.Buddies[buddyID] then
 		return buddyID
 	end
@@ -23,7 +20,7 @@ end
 function GM.Buddy:GetPlayerByBuddyID( intBuddyID )
 	for k, v in pairs( player.GetAll() ) do
 		if not v:GetCharacterID() then continue end
-		if v:GetCharacterID() == tonumber( intBuddyID ) then
+		if v:SteamID() == intBuddyID then
 			return v
 		end
 	end
@@ -37,34 +34,64 @@ end
 
 function GM.Buddy:IsPlayerBuddyWith( pPlayer, pOther )
 	if not IsValid( pOther ) then return false end
-	
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not saveTable.Buddies then return false end
 	if not pOther:GetCharacterID() then return false end
 
-	return saveTable.Buddies[pOther:GetCharacterID()] or false
+	return saveTable.Buddies[pOther:SteamID()] or false
 end
 
 function GM.Buddy:IsCarShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareCar
+	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareCar
 end
 
 function GM.Buddy:IsDoorShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareDoors
+	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareDoors
 end
 
 function GM.Buddy:IsItemShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareItems
+	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareItems
 end
 
-function GM.Buddy:PlayerAddBuddy( pPlayer, intBuddyID )
+
+function GM.Buddy:PlayerAddBuddy( pPlayer, intBuddyID )-- ASCP POI
 	local otherPlayer = self:GetPlayerByBuddyID( intBuddyID )
 	if not IsValid( otherPlayer ) then return end
-	
-	if pPlayer == otherPlayer or not IsValid( otherPlayer ) then return end
+	net.Start("santos_askbuddy")
+	net.WriteEntity(pPlayer)
+	net.WriteString(intBuddyID)
+	net.Send(otherPlayer)
+	print("Add Buddy")
+end
+
+function santosBuddy( otherPlayer, pPlayer, intBuddyID )
+	if not IsValid( otherPlayer ) then return end
+	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
+	if not saveTable or not otherPlayer:GetCharacterID() then return end
+	saveTable.Buddies = saveTable.Buddies or {}
+	saveTable.Buddies[intBuddyID] = {
+		Settings = {
+			ShareCar = false,
+			ShareDoors = false,
+			ShareItems = false,
+		},
+		LastName = otherPlayer:Nick()
+	}
+	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
+	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
+	print("Stage 1")
+end
+
+util.AddNetworkString("santos_acceptedbuddy")
+net.Receive("santos_acceptedbuddy",function(pl)
+	local otherPlayer = net.ReadEntity()
+	local pPlayer = net.ReadEntity() 
+	local intBuddyID =  otherPlayer:SteamID()
+	local meuid = pPlayer:SteamID()
+	if not IsValid( otherPlayer ) then return end
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not otherPlayer:GetCharacterID() then return end
 
@@ -80,8 +107,9 @@ function GM.Buddy:PlayerAddBuddy( pPlayer, intBuddyID )
 
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	--GAMEMODE.SQL:InsdupCharacterDataStoreVar( pPlayer:GetCharacterID(), "Buddies", saveTable.Buddies )
-end
+	santosBuddy(pPlayer, otherPlayer, meuid)
+	print("santos_acceptedbuddy")
+end)
 
 function GM.Buddy:PlayerRemoveBuddy( pPlayer, intBuddyID )
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
@@ -89,7 +117,8 @@ function GM.Buddy:PlayerRemoveBuddy( pPlayer, intBuddyID )
 	saveTable.Buddies[intBuddyID] = nil
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	--GAMEMODE.SQL:InsdupCharacterDataStoreVar( pPlayer:GetCharacterID(), "Buddies", saveTable.Buddies )
+	local meuid = pPlayer:SteamID()
+	local otherPlayer = self:GetPlayerByBuddyID( intBuddyID )
 end
 
 function GM.Buddy:PlayerUpdateBuddyKey( pPlayer, intBuddyID, strKey, vaValue )
@@ -102,7 +131,6 @@ function GM.Buddy:PlayerUpdateBuddyKey( pPlayer, intBuddyID, strKey, vaValue )
 	saveTable.Buddies[intBuddyID].Settings[strKey] = vaValue
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	--GAMEMODE.SQL:InsdupCharacterDataStoreVar( pPlayer:GetCharacterID(), "Buddies", saveTable.Buddies )
 end
 
 hook.Add( "GamemodePlayerSelectCharacter", "SendBuddyData", function( pPlayer )
