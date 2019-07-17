c0e8d1249a85d784f1a73a42ebee7828b7a484a1
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-17 20:18:01 -0300

    Máscaras Stencil

---
+++ gamemode/libs/cl_stencil.lua
@@ -0,0 +1,53 @@
+function AwMaskInverse(maskFn, drawFn)
+    render.ClearStencil()
+    render.SetStencilEnable(true)
+
+    render.SetStencilWriteMask(1)
+    render.SetStencilTestMask(1)
+
+    render.SetStencilFailOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilPassOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilZFailOperation(STENCILOPERATION_ZERO)
+    render.SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_NEVER)
+    render.SetStencilReferenceValue(1)
+
+    maskFn()
+
+    render.SetStencilFailOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilPassOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilZFailOperation(STENCILOPERATION_ZERO)
+    render.SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_EQUAL)
+    render.SetStencilReferenceValue(0)
+
+    drawFn()
+
+    render.SetStencilEnable(false)
+    render.ClearStencil()
+end
+
+function AwMask(maskFn, drawFn)
+    render.ClearStencil()
+    render.SetStencilEnable(true)
+
+    render.SetStencilWriteMask(1)
+    render.SetStencilTestMask(1)
+
+    render.SetStencilFailOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilPassOperation(STENCILOPERATION_ZERO)
+    render.SetStencilZFailOperation(STENCILOPERATION_ZERO)
+    render.SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_NEVER)
+    render.SetStencilReferenceValue(1)
+
+    maskFn()
+
+    render.SetStencilFailOperation(STENCILOPERATION_ZERO)
+    render.SetStencilPassOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilZFailOperation(STENCILOPERATION_ZERO)
+    render.SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_EQUAL)
+    render.SetStencilReferenceValue(1)
+
+    drawFn()
+
+    render.SetStencilEnable(false)
+    render.ClearStencil()
+end
