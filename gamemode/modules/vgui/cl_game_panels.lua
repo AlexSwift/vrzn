42b03b36e915bb151eae84cef0e4265c63b88a45
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-18 14:37:51 -0300

    Novo menu (GAMEPANEL)

--- gamemode/modules/vgui/cl_game_panels.lua
+++ gamemode/modules/vgui/cl_game_panels.lua
@@ -1979,3 +1979,345 @@ vgui.Register("AWESOME_Grid", PANEL, "DPanel")
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local PANEL = {}
+
+AccessorFunc( PANEL, "m_bBorder",			"DrawBorder" )
+AccessorFunc( PANEL, "m_bDeleteSelf",		"DeleteSelf" )
+AccessorFunc( PANEL, "m_iMinimumWidth",		"MinimumWidth" )
+AccessorFunc( PANEL, "m_bDrawColumn",		"DrawColumn" )
+AccessorFunc( PANEL, "m_iMaxHeight",		"MaxHeight" )
+
+AccessorFunc( PANEL, "m_pOpenSubMenu",		"OpenSubMenu" )
+
+function PANEL:Init()
+
+	self:SetIsMenu( true )
+	self:SetDrawBorder( true )
+	self:SetPaintBackground( true )
+	self:SetMinimumWidth( 100 )
+	self:SetDrawOnTop( true )
+	self:SetMaxHeight( ScrH() * 0.9 )
+	self:SetDeleteSelf( true )
+
+	self:SetPadding( 0 )
+
+	-- Automatically remove this panel when menus are to be closed
+	RegisterDermaMenuForClose( self )
+
+end
+
+function PANEL:AddPanel( pnl )
+
+	self:AddItem( pnl )
+	pnl.ParentMenu = self
+
+end
+
+function PANEL:AddOption( strText, funcFunction )
+
+	local pnl = vgui.Create( "DMenuOption", self )
+	pnl:SetMenu( self )
+	pnl:SetTextColor( Color(255,255,255) )
+	pnl:SetText( strText )
+	pnl.OnCursorEntered = function(pnl)
+		pnl.Paint = function(pnl)
+			pnl:SetTextColor( Color(0,0,0) )
+			surface.SetDrawColor(255, 255, 255, 255)
+			surface.SetMaterial( Material("materials/vgui/elements/gradient-left.png") ) 
+			surface.DrawTexturedRect(0, 0, pnl:GetWide(), pnl:GetTall() )
+		end
+	end
+	pnl.OnCursorExited = function(pnl)
+		pnl.Paint = function(pnl)
+			pnl:SetTextColor( Color(255,255,255) )
+		end
+	end
+
+	if ( funcFunction ) then pnl.DoClick = funcFunction end
+
+	self:AddPanel( pnl )
+
+	return pnl
+
+end
+
+function PANEL:AddCVar( strText, convar, on, off, funcFunction )
+
+	local pnl = vgui.Create( "DMenuOptionCVar", self )
+	pnl:SetMenu( self )
+	pnl:SetText( strText )
+	if ( funcFunction ) then pnl.DoClick = funcFunction end
+
+	pnl:SetConVar( convar )
+	pnl:SetValueOn( on )
+	pnl:SetValueOff( off )
+
+	self:AddPanel( pnl )
+
+	return pnl
+
+end
+
+function PANEL:AddSpacer( strText, funcFunction )
+
+	local pnl = vgui.Create( "DPanel", self )
+	pnl.Paint = function( p, w, h )
+		derma.SkinHook( "Paint", "MenuSpacer", p, w, h )
+	end
+
+	pnl:SetTall( 1 )
+	self:AddPanel( pnl )
+
+	return pnl
+
+end
+
+function PANEL:AddSubMenu( strText, funcFunction )
+
+	local pnl = vgui.Create( "DMenuOption", self )
+	local SubMenu = pnl:AddSubMenu( strText, funcFunction )
+
+	pnl:SetText( strText )
+	if ( funcFunction ) then pnl.DoClick = funcFunction end
+
+	self:AddPanel( pnl )
+
+	return SubMenu, pnl
+
+end
+
+function PANEL:Hide()
+
+	local openmenu = self:GetOpenSubMenu()
+	if ( openmenu ) then
+		openmenu:Hide()
+	end
+
+	self:SetVisible( false )
+	self:SetOpenSubMenu( nil )
+
+end
+
+function PANEL:OpenSubMenu( item, menu )
+
+	-- Do we already have a menu open?
+	local openmenu = self:GetOpenSubMenu()
+	if ( IsValid( openmenu ) && openmenu:IsVisible() ) then
+
+		-- Don't open it again!
+		if ( menu && openmenu == menu ) then return end
+
+		-- Close it!
+		self:CloseSubMenu( openmenu )
+
+	end
+
+	if ( !IsValid( menu ) ) then return end
+
+	local x, y = item:LocalToScreen( self:GetWide(), 0 )
+	menu:Open( x - 3, y, false, item )
+
+	self:SetOpenSubMenu( menu )
+
+end
+
+function PANEL:CloseSubMenu( menu )
+
+	menu:Hide()
+	self:SetOpenSubMenu( nil )
+
+end
+
+function PANEL:Paint( w, h )
+
+	render.ClearStencil()
+	render.SetStencilEnable( true )
+	render.SetStencilReferenceValue( 1 )
+	render.SetStencilTestMask( 1 )
+	render.SetStencilWriteMask( 1 )
+
+	render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_NEVER )
+	render.SetStencilFailOperation( STENCILOPERATION_REPLACE )
+	render.SetStencilPassOperation( STENCILOPERATION_REPLACE )
+	render.SetStencilZFailOperation( STENCILOPERATION_REPLACE )
+
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	surface.DrawRect( 0, 0, w, h )
+
+	render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_EQUAL )
+	render.SetStencilFailOperation( STENCILOPERATION_KEEP )
+	render.SetStencilPassOperation( STENCILOPERATION_KEEP )
+	render.SetStencilZFailOperation( STENCILOPERATION_KEEP )
+		surface.SetMaterial( MAT_BLUR )
+		surface.SetDrawColor( 255, 255, 255, 255 )
+
+		for i = 0, 1, 0.33 do
+			MAT_BLUR:SetFloat( '$blur', 5 *i )
+			MAT_BLUR:Recompute()
+			render.UpdateScreenEffectTexture()
+
+			local x, y = self:GetPos()
+			surface.DrawTexturedRect( -x, -y, ScrW(), ScrH() )
+		end
+	render.SetStencilEnable( false )
+
+	surface.SetDrawColor( 26, 26, 26, 30 )
+	surface.DrawRect( 0, 0, w, h )
+
+	-- if ( !self:GetPaintBackground() ) then return end
+
+	-- -- derma.SkinHook( "Paint", "Menu", self, w, h )
+	-- return true
+
+end
+
+function PANEL:ChildCount()
+	return #self:GetCanvas():GetChildren()
+end
+
+function PANEL:GetChild( num )
+	return self:GetCanvas():GetChildren()[ num ]
+end
+
+function PANEL:PerformLayout()
+
+	local w = self:GetMinimumWidth()
+
+	-- Find the widest one
+	for k, pnl in pairs( self:GetCanvas():GetChildren() ) do
+
+		pnl:PerformLayout()
+		w = math.max( w, pnl:GetWide() )
+
+	end
+
+	self:SetWide( w )
+
+	local y = 0 -- for padding
+
+	for k, pnl in pairs( self:GetCanvas():GetChildren() ) do
+
+		pnl:SetWide( w )
+		pnl:SetPos( 0, y )
+		pnl:InvalidateLayout( true )
+
+		y = y + pnl:GetTall()
+
+	end
+
+	y = math.min( y, self:GetMaxHeight() )
+
+	self:SetTall( y )
+
+	derma.SkinHook( "Layout", "Menu", self )
+
+	DScrollPanel.PerformLayout( self )
+
+end
+
+--[[---------------------------------------------------------
+	Open - Opens the menu.
+	x and y are optional, if they're not provided the menu
+		will appear at the cursor.
+-----------------------------------------------------------]]
+function PANEL:Open( x, y, skipanimation, ownerpanel )
+
+	RegisterDermaMenuForClose( self )
+
+	local maunal = x && y
+
+	x = x or gui.MouseX()
+	y = y or gui.MouseY()
+
+	local OwnerHeight = 0
+	local OwnerWidth = 0
+
+	if ( ownerpanel ) then
+		OwnerWidth, OwnerHeight = ownerpanel:GetSize()
+	end
+
+	self:PerformLayout()
+
+	local w = self:GetWide()
+	local h = self:GetTall()
+
+	self:SetSize( w, h )
+
+	if ( y + h > ScrH() ) then y = ( ( maunal && ScrH() ) or ( y + OwnerHeight ) ) - h end
+	if ( x + w > ScrW() ) then x = ( ( maunal && ScrW() ) or x ) - w end
+	if ( y < 1 ) then y = 1 end
+	if ( x < 1 ) then x = 1 end
+
+	self:SetPos( x, y )
+
+	-- Popup!
+	self:MakePopup()
+
+	-- Make sure it's visible!
+	self:SetVisible( true )
+
+	-- Keep the mouse active while the menu is visible.
+	self:SetKeyboardInputEnabled( false )
+
+end
+
+--
+-- Called by DMenuOption
+--
+function PANEL:OptionSelectedInternal( option )
+
+	self:OptionSelected( option, option:GetText() )
+
+end
+
+function PANEL:OptionSelected( option, text )
+
+	-- For override
+
+end
+
+function PANEL:ClearHighlights()
+
+	for k, pnl in pairs( self:GetCanvas():GetChildren() ) do
+		pnl.Highlight = nil
+	end
+
+end
+
+function PANEL:HighlightItem( item )
+
+	for k, pnl in pairs( self:GetCanvas():GetChildren() ) do
+		if ( pnl == item ) then
+			pnl.Highlight = true
+		end
+	end
+
+end
+vgui.Register("AwMenu", PANEL, "DScrollPanel")
