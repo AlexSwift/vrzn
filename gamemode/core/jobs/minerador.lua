a68ce1c398a04e9096de784c497c52ed768b197d
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-20 04:44:40 -0300

    Job minerador

---
+++ gamemode/core/jobs/minerador.lua
@@ -0,0 +1,57 @@
+--[[
+	Name: civilian.lua
+	For: TalosLife
+	By: TalosLife
+]]--
+
+local Job = {}
+Job.ID = 22
+Job.Enum = "JOB_MINER"
+Job.TeamColor = Color( 110, 48, 0 )
+Job.Name = "Minerador"
+Job.Cat = "citizen"
+Job.Pay = {
+	{ PlayTime = 0, Pay = 15 },
+	{ PlayTime = 0, Pay = 20 },
+	{ PlayTime = 0, Pay = 25 },
+	{ PlayTime = 0, Pay = 30 },
+}
+
+function Job:OnPlayerJoinJob( pPlayer )
+end
+
+function Job:OnPlayerQuitJob( pPlayer )
+end
+
+function Job:GetPlayerModel( pPlayer )
+	local base, overload
+
+	if SERVER then
+		base = GAMEMODE.Player:GetGameVar( pPlayer, "char_model_base", nil )
+		overload = GAMEMODE.Player:GetGameVar( pPlayer, "char_model_overload", nil )
+	else
+		base = GAMEMODE.Player:GetGameVar( "char_model_base", nil )
+		overload = GAMEMODE.Player:GetGameVar( "char_model_overload", nil )
+	end
+	
+	if util.IsValidModel( overload or "" ) then
+		return overload
+	else
+		return base
+	end
+end
+
+function Job:PlayerSetModel( pPlayer )
+	local mdl = self:GetPlayerModel(pPlayer)
+
+	if mdl then
+		pPlayer:SetModel( mdl )
+		pPlayer:SetSkin( GAMEMODE.Player:GetGameVar(pPlayer, "char_skin", 0) )
+	end
+end
+
+function Job:PlayerLoadout( pPlayer )
+	pPlayer:Give( "zrms_pickaxe" )
+end
+
+GM.Jobs:Register( Job )
