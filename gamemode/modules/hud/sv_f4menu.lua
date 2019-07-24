f558f33ced02620fa99171c38d57c8c0b5976068
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-23 22:01:09 -0300

    Menu de interação

--- gamemode/modules/hud/sv_f4menu.lua
+++ gamemode/modules/hud/sv_f4menu.lua
@@ -4,31 +4,12 @@ function GM:ShowSpare2(ply)
     ply:ConCommand("open_f4_menu")
 end
 
-function AddNoteAll()
+function AddNoteAll(ply, msg)
+    -- ply:AddNote()
 end
--- local status = net.ReadBit()
 
--- 	if (F4Menu == nil) then
--- 		F4Menu = vgui.Create( "AWESOME_Frame" )
--- 		F4Menu:SetSize( 932, 600 )
--- 		F4Menu:Center()
--- 		F4Menu:SetTitle("VRZN Menu")
--- 		F4Menu:MakePopup()
--- 		-- F4Menu:SetDeleteOnClose(false)
+function GM:ShowTeam(ply)
+    ply:ConCommand("open_f2_menu")
+end
 
--- 		F4NavBar = vgui.Create( "AWESOME_Sidebar", F4Menu )
--- 		F4NavBar:SetBody(F4Menu)
--- 		F4NavBar:Dock( LEFT ) 
--- 		F4NavBar:AddMenu("jobs", "Lista de Jobs", "AW_f4_JOBS")
--- 		F4NavBar:AddMenu( "entities", "Loja de itens","AW_f4_SHOP")
--- 		F4NavBar:AddMenu( "commands", "Lista de comandos","AW_f4_COMMANDS")
--- 		F4NavBar:SetActive( 1 )
--- 	end
-
--- 	if (status == 0) then
--- 		F4Menu:SetVisible(false)
--- 		gui.EnableScreenClicker(false)
--- 	else
--- 		F4Menu:SetVisible(true)
--- 		gui.EnableScreenClicker(true)
-	-- end
+-- concommand.Add("radial door", function())
