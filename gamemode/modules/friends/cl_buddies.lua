59f3751902be6523274beb23287d39c60d73ea1a
Matheus alves <matheus.silva3@ftc.edu.br>
2019-07-23 22:00:19 -0300

    sistema amigos

--- gamemode/modules/friends/cl_buddies.lua
+++ gamemode/modules/friends/cl_buddies.lua
@@ -100,3 +100,142 @@ function GM.Buddy:PostDrawTranslucentRenderables()
 		end
 	end
 end
+
+
+
+	---------------------------------------------------
+	-- FONTS
+	---------------------------------------------------
+	surface.CreateFont( "Roboto_Title", { 
+		font = "Roboto", 
+		extended = false,
+		size = 25,
+		weight = 800,
+		antialias = true,
+	})
+	
+	surface.CreateFont( "Roboto_Button", {
+		font = "Roboto", 
+		extended = false,
+		size = 15,
+		weight = 500,
+		antialias = true,
+	})
+	
+	surface.CreateFont( "Roboto_Subtext", {
+		font = "Roboto",
+		extended = false,
+		size = 18,
+		weight = 400,
+		antialias = true,
+	})	
+	
+	---------------------------------------------------
+	-- POPUP CONFIGURATION
+	---------------------------------------------------
+	local main_color = Color(32, 34, 37)
+	local secondary_color = Color(54, 57, 62) 
+	local text_color = Color(255, 255, 255)
+	
+	local button_color = Color(47, 49, 54)
+	local button_hovered_color = Color(66, 70, 77)
+	
+	local header_color = Color(255, 255, 255)
+	
+	local subtext_color = Color(0, 195, 165)
+	local subtext_message = ""
+	
+	local width = 350
+	local height = 135
+	
+	local disable_multicore_button = "Deny"
+	
+		
+	local function MakePopupWindow()
+	
+		local parceiro = net.ReadEntity()
+		local id = net.ReadInt(16)
+		local header_message = "The player [#"..parceiro:Nick().."] "..parceiro:Name().." Want's to add you as a friend"
+		local popup = vgui.Create("DFrame")
+		popup:SetTitle("Add friend")
+		popup:SetSize( width, height )
+		popup:SetPos( 0, 0 )
+		popup:MakePopup()
+		popup:SetCursor("crosshair")
+		popup:SetKeyboardInputEnabled(false)
+		popup:SetMouseInputEnabled(true)
+		popup:ShowCloseButton(false)
+		function popup.Paint(s, w, h)
+			draw.RoundedBox(0, 0, 0, w, h, main_color)
+			draw.RoundedBox(0, 1, 1, w - 2, h - 2, secondary_color)
+		end
+	
+		local btns = vgui.Create("DPanel", popup)
+		btns:SetDrawBackground(false)
+		btns:Dock(BOTTOM)
+		btns:DockMargin(4, 4, 4, 4)
+				
+		local title = vgui.Create("DLabel", popup)
+		title:SetText(header_message)
+		title:SetFont("Roboto_Title")
+		title:SetPos( 0, 0 )
+		title:SetTextColor( text_color )
+		title:SetContentAlignment(8)
+		title:Dock(FILL)
+		title:DockMargin(0, 0, 0, 0)
+	
+		local subtext = vgui.Create("DLabel", popup)
+		subtext:SetText( header_message )
+		subtext:SetFont("Roboto_Subtext")
+		subtext:SetPos( 0, 0 )
+		subtext:SetTextColor( subtext_color )
+		subtext:SetContentAlignment(5)
+		subtext:Dock(FILL)
+		subtext:DockMargin(0, 0, 0, 0)
+			
+		local button_enable = vgui.Create("DButton", btns)
+		button_enable:SetText( "Aceitar" )
+		button_enable:SetFont("Roboto_Button")
+		button_enable:SetPos( 0, 0 )
+		button_enable:SetTextColor( header_color )
+		button_enable:SetWide(popup:GetWide() * 0.5 - 14)
+		button_enable:Dock(LEFT)
+		function button_enable.Paint(s, w, h)
+			draw.RoundedBox(0, 0, 0, w, h, button_color)
+			
+			if (s.Hovered) then
+				draw.RoundedBox(0, 0, 0, w, h, button_hovered_color)																																																			-- Copyright 76561198110511213
+			end
+		end
+		button_enable:SetTextColor( text_color )
+		button_enable.DoClick = function()
+			ply = LocalPlayer()
+			net.Start("frp_AcceptedBuddy")
+				net.WriteEntity(ply)
+				net.WriteEntity(parceiro)
+				net.WriteInt(id,16)
+			net.SendToServer()
+			popup:Remove()
+		end
+	
+		local button_disable = vgui.Create("DButton", btns)
+		button_disable:SetText( disable_multicore_button )
+		button_disable:SetFont("Roboto_Button")
+		button_disable:SetPos( 0, 0 )
+		button_disable:SetWide(popup:GetWide() * 0.5 - 14)
+		button_disable:Dock(RIGHT)
+		function button_disable.Paint(s, w, h)
+			draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
+			
+			if (s.Hovered) then
+				draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+			end
+		end
+		button_disable:SetTextColor( text_color )
+		button_disable.DoClick = function()
+			popup:Remove()
+		end
+		
+	end	
+	net.Receive("frp_askbuddy",MakePopupWindow)
+	
--- gamemode/modules/friends/sv_buddies.lua
+++ gamemode/modules/friends/sv_buddies.lua
@@ -3,15 +3,14 @@
 	For: TalosLife
 	By: TalosLife
 ]]--
-
-util.AddNetworkString("santos_askbuddy")-- Send Friend Request
+util.AddNetworkString("frp_askbuddy")-- ASCP POI
 GM.Buddy = {}
 
 function GM.Buddy:GetPlayerBuddyID( pPlayer, pOther )
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not saveTable.Buddies then return end
 
-	local buddyID = pOther:SteamID()
+	local buddyID = pOther:GetCharacterID()
 	if saveTable.Buddies[buddyID] then
 		return buddyID
 	end
@@ -20,7 +19,7 @@ end
 function GM.Buddy:GetPlayerByBuddyID( intBuddyID )
 	for k, v in pairs( player.GetAll() ) do
 		if not v:GetCharacterID() then continue end
-		if v:SteamID() == intBuddyID then
+		if v:GetCharacterID() == tonumber( intBuddyID ) then
 			return v
 		end
 	end
@@ -34,40 +33,45 @@ end
 
 function GM.Buddy:IsPlayerBuddyWith( pPlayer, pOther )
 	if not IsValid( pOther ) then return false end
+	
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not saveTable.Buddies then return false end
 	if not pOther:GetCharacterID() then return false end
 
-	return saveTable.Buddies[pOther:SteamID()] or false
+	return saveTable.Buddies[pOther:GetCharacterID()] or false
 end
 
 function GM.Buddy:IsCarShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareCar
+	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareCar
 end
 
 function GM.Buddy:IsDoorShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareDoors
+	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareDoors
 end
 
 function GM.Buddy:IsItemShared( pPlayer, pOwner )
 	if not self:IsPlayerBuddyWith( pOwner, pPlayer ) then return false end
-	return self:GetPlayerBuddyData( pOwner, pPlayer:SteamID() ).Settings.ShareItems
+	return self:GetPlayerBuddyData( pOwner, pPlayer:GetCharacterID() ).Settings.ShareItems
 end
-
 
 function GM.Buddy:PlayerAddBuddy( pPlayer, intBuddyID )-- ASCP POI
 	local otherPlayer = self:GetPlayerByBuddyID( intBuddyID )
 	if not IsValid( otherPlayer ) then return end
-	net.Start("santos_askbuddy")
+	net.Start("frp_askbuddy")
 	net.WriteEntity(pPlayer)
-	net.WriteString(intBuddyID)
+	net.WriteInt(intBuddyID,16)
 	net.Send(otherPlayer)
-	print("Add Buddy")
+	print("ID requisitado: "..intBuddyID)
+	print("====================")
 end
 
-function santosBuddy( otherPlayer, pPlayer, intBuddyID )
+function ascp1337( otherPlayer, pPlayer,intBuddyID )
+	print("recursividade")
+	print(pPlayer)
+	print(otherPlayer)
+	print("id do cliente que foi requisitado: "..intBuddyID)
 	if not IsValid( otherPlayer ) then return end
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not otherPlayer:GetCharacterID() then return end
@@ -82,15 +86,19 @@ function santosBuddy( otherPlayer, pPlayer, intBuddyID )
 	}
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	print("Stage 1")
 end
 
-util.AddNetworkString("santos_acceptedbuddy")
-net.Receive("santos_acceptedbuddy",function(pl)
+util.AddNetworkString("frp_AcceptedBuddy")--ASCP POI--
+net.Receive("frp_AcceptedBuddy",function(pl)
+	print("aceitou \n")
+	--intBuddyID = net.ReadInt(16)
 	local otherPlayer = net.ReadEntity()
 	local pPlayer = net.ReadEntity() 
-	local intBuddyID =  otherPlayer:SteamID()
-	local meuid = pPlayer:SteamID()
+	intBuddyID	=  net.ReadInt(16)
+	print(pPlayer)
+	print(otherPlayer)
+	print("id do cliente que requisitou:",intBuddyID)
+	local meuid = GAMEMODE.Player:GetSharedGameVar(pPlayer, "char_id", "") 
 	if not IsValid( otherPlayer ) then return end
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not otherPlayer:GetCharacterID() then return end
@@ -104,21 +112,33 @@ net.Receive("santos_acceptedbuddy",function(pl)
 		},
 		LastName = otherPlayer:Nick()
 	}
-
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	santosBuddy(pPlayer, otherPlayer, meuid)
-	print("santos_acceptedbuddy")
+	ascp1337(pPlayer, otherPlayer, meuid)
 end)
-
-function GM.Buddy:PlayerRemoveBuddy( pPlayer, intBuddyID )
+function ascp1337pt2( pPlayer, intBuddyID )
+	print("Jogador 2: \n =======")
+	print(pPlayer)
+	print(intBuddyID)
 	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
 	if not saveTable or not saveTable.Buddies then return end
 	saveTable.Buddies[intBuddyID] = nil
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
-	local meuid = pPlayer:SteamID()
+end
+function GM.Buddy:PlayerRemoveBuddy( pPlayer, intBuddyID )
+	print("Remover parceiro \n Jogador 1:\n ========")
+	print(pPlayer)
+	print(intBuddyID)--
+	local saveTable = GAMEMODE.Char:GetCurrentSaveTable( pPlayer )
+	if not saveTable or not saveTable.Buddies then return end
+	saveTable.Buddies[intBuddyID] = nil
+	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
+	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
+	local meuid = GAMEMODE.Player:GetSharedGameVar(pPlayer, "char_id", "")
 	local otherPlayer = self:GetPlayerByBuddyID( intBuddyID )
+	ascp1337pt2(otherPlayer, meuid)
+	--GAMEMODE.SQL:InsdupCharacterDataStoreVar( pPlayer:GetCharacterID(), "Buddies", saveTable.Buddies )
 end
 
 function GM.Buddy:PlayerUpdateBuddyKey( pPlayer, intBuddyID, strKey, vaValue )
@@ -131,6 +151,7 @@ function GM.Buddy:PlayerUpdateBuddyKey( pPlayer, intBuddyID, strKey, vaValue )
 	saveTable.Buddies[intBuddyID].Settings[strKey] = vaValue
 	GAMEMODE.Net:SendBuddyUpdate( pPlayer, intBuddyID )
 	GAMEMODE.SQL:MarkDiffDirty( pPlayer, "data_store", "Buddies" )
+	--GAMEMODE.SQL:InsdupCharacterDataStoreVar( pPlayer:GetCharacterID(), "Buddies", saveTable.Buddies )
 end
 
 hook.Add( "GamemodePlayerSelectCharacter", "SendBuddyData", function( pPlayer )
